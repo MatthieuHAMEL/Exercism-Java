83ce8eb056cebd7d9813294b7408cc2037d380f9
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-03-30 18:47:36 +0200

    Exercise 30 (Dot DSL)

---
+++ dot-dsl/src/main/java/Graph.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+    private final Map<String, String> attributes;
+    private List<Node> nodes;
+    private List<Edge> edges;
+
+    public Graph() {
+        this(new HashMap<>());
+    }
+
+    public Graph(Map<String, String> attributes) {
+        this.attributes = attributes;
+        this.nodes = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
+
+    public Collection<Node> getNodes() {
+       return this.nodes;
+    }
+
+    public Collection<Edge> getEdges() {
+        return this.edges;
+    }
+
+    public Graph node(String name) {
+        this.nodes.add(new Node(name));
+        return this;
+    }
+
+    public Graph node(String name, Map<String, String> attributes) {
+        this.nodes.add(new Node(name, attributes));
+        return this;
+    }
+
+    public Graph edge(String start, String end) {
+        this.edges.add(new Edge(start, end));
+        return this;
+    }
+
+    public Graph edge(String start, String end, Map<String, String> attributes) {
+        this.edges.add(new Edge(start, end, attributes));
+        return this;
+    }
+
+    public Map<String, String> getAttributes() {
+        return this.attributes;
+    }
+}
