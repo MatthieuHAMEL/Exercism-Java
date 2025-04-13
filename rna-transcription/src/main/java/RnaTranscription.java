218fb41a85c6493d5cf62e4f4cb17ce8c9826554
HAMEL <75175897+MatthieuHAMEL@users.noreply.github.com>
2025-04-13 17:37:52 +0200

    Exercise 36 (RNA transcription)

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,27 @@
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < dnaStrand.length(); i++) {
+            switch (dnaStrand.charAt(i)) {
+                case 'G':
+                    sb.append('C');
+                    break;
+                case 'C':
+                    sb.append('G');
+                    break;
+                case 'T':
+                    sb.append('A');
+                    break;
+                case 'A':
+                    sb.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("A/T/G/C expected.");
+            }
+        }
+        return sb.toString();
+        
+    }
+
+}
